f649b93dac45f0e6b01627cc0c3fc3380e4ba704
Julius Koskela <julkoske@gmail.com>
2020-08-26 16:14:09 +0300

    //J

--- fillit.c
+++ fillit.c
@@ -6,74 +6,91 @@
 /*   By: jkoskela <jkoskela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 12:44:16 by jkoskela          #+#    #+#             */
-/*   Updated: 2020/08/26 12:56:23 by jkoskela         ###   ########.fr       */
+/*   Updated: 2020/08/26 16:14:04 by jkoskela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitlib/bitlib.h"
 #include "libft/libft.h"
 
-int			main(void)
+typedef struct		s_tetriminoe
 {
+	uint64_t		*row;
+	size_t			w;
+	size_t			h;
+}					t_tet;
 
-	size_t n = 0;
-	size_t i = 0;
-	uint64_t			*field = bf_init(8);
+typedef struct		s_field
+{
+	uint64_t		*row;
+	size_t			size;
+}					t_field;
 
-	uint64_t			*field = bf_init(8);
+uint64_t		check_rest(t_field *field, t_tet *piece, size_t i)
+{
+	size_t		j;
 
-							field[0] = b("11111110");
-							field[1] = b("10000000");
-							field[2] = b("00000000");
-							field[3] = b("00000000");
-							field[4] = b("00000000");
-							field[5] = b("00000000");
-							field[6] = b("00000000");
-							field[7] = b("00000000");
-
-	uint64_t			*piece = bf_init(2);
-
-							piece[0] = b("11");
-							piece[1] = b("11");
-
-	solver(field, piece);
-	ft_putchar('\n');
-	printbits(bf_column(field, 2, 32), 32);
-	ft_putchar('\n');
-	printf("%llu\n", bf_column(field, 2, 32));
-
-							field[0] = b("11111110");
-							field[1] = b("10000000");
-							field[2] = b("00000000");
-							field[3] = b("00000000");
-							field[4] = b("00000000");
-							field[5] = b("00000000");
-							field[6] = b("00000000");
-							field[7] = b("00000000");
-
-	uint64_t			*piece = bf_init(2);
-
-							piece[0] = b("11");
-							piece[1] = b("11");
-
-	while ((field[n] & piece[0]) > 0)
+	j = 1;
+	while (j > piece->h)
 	{
-		if (i == 6)
+		piece->row[0] == piece->row[0] << i;
+	}
+}
+int		solver(t_field *field, t_tet *piece, size_t i)
+{
+	size_t	n = 0;
+	int		r = 0;
+	uint64_t tmp = piece->row[0];
+
+	// Check piece 1st row against corresponding row of field.
+	if (tmp & field->row[n] != 0 && i <= field->size - piece->w)
+	{
+		tmp = tmp << 1;
+		solver(field, piece, i + 1);
+	}
+	else
+	{
+		if (check_rest(field, piece, i))
 		{
-			piece[0] = b("011");
-			piece[1] = b("110");
-			n++;
-			i = 0;
-		}
-		else
-		{
-			piece[0] = piece[0] << 1;
-			piece[1] = piece[1] << 1;
-			i++;
+			field->row[n]  = field->row[n] | piece->row[0];
+			field->row[n + 1] = field->row[n + 1] | piece->row[1];
+			return(1);
 		}
 	}
-	field[n] = field[n] | piece[0];
-	field[n + 1] = field[n + 1] | piece[1];
-	bf_print(field, 8);
+	// If the 1st row matches check the other rows of piece. If all rows match
+	// save identity of piece on the board and return 1.
+	// Else return 0 and go back to previous step.
+	return (0);
+}
+
+int			main(void)
+{
+	size_t		n = 0;
+	size_t		i = 0;
+	t_field		field;
+
+				field.row = bf_init(8);
+				field.row[0] = b("11111110");
+				field.row[1] = b("10000000");
+				field.row[2] = b("00000000");
+				field.row[3] = b("00000000");
+				field.row[4] = b("00000000");
+				field.row[5] = b("00000000");
+				field.row[6] = b("00000000");
+				field.row[7] = b("00000000");
+
+				field.size = 8;
+
+	t_tet		block1;
+
+				block1.row = (int *)malloc(sizeof(int) * 2);
+				block1.row[0] = b("011");
+				block1.row[1] = b("110");
+
+				block1.w = 3;
+				block1.h = 2;
+
+	solver(&field, &block1, 0);
+	//bf_print(field, 8);
 	return (0);
 }
--- solver.c
+++ solver.c
@@ -13,28 +13,41 @@
 #include "bitlib.h"
 #include "../libft/libft.h"
 
-uint64_t		solver(uint64_t *field, uint64_t *piece)
+uint64_t		check_other_rows(uint64_t *field, uint64_t *piece, size_t i, size_t n)
 {
-	int n = 0;
-	int i = 0;
-	while ((field[n] & piece[0]) > 0)
+	size_t		j;
+
+	j = 1;
+	while (j > piece_height)
 	{
-		if (i == 6)
-		{
-			piece[0] = b("11");
-			piece[1] = b("11");
-			n++;
-			i = 0;
-		}
-		else
-		{
-			piece[0] = piece[0] << 1;
-			piece[1] = piece[1] << 1;
-			i++;
-		}
+		piece[0] == piece[0] << i;
 	}
-	field[n] 	 = field[n]		| piece[0];
+}
+uint64_t		solver(uint64_t *field, uint64_t *piece, size_t size, size_t i)
+{
+	size_t	n = 0;
+	int		r = 0;
+
+	// Check piece 1st row against corresponding row of field.
+	if (piece[0] & field[n] != 0)
+	{
+		piece[0] == piece[0] << 1;
+		solver(field, piece, size, i + 1);
+	}
+	else
+	{
+		if (check_other_rows(field, piece, size, i))
+			return(1);
+	}
+
+	// If the 1st row matches check the other rows of piece. If all rows match
+	// save identity of piece on the board and return 1.
+	// Else return 0 and go back to previous step.
+
+	field[n]  = field[n] | piece[0];
 	field[n + 1] = field[n + 1] | piece[1];
 	bf_print(field, 8);
 	return (0);
 }
+
+
