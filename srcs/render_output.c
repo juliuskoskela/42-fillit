2e4d5198961b24db89c041257d1299845a1f22c6
Julius Koskela <julkoske@gmail.com>
2020-09-12 19:18:33 +0300

    read_input work //J

--- srcs/render_output.c
+++ srcs/render_output.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   render_output.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jkoskela <jkoskela@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/09 05:30:06 by jkoskela          #+#    #+#             */
-/*   Updated: 2020/09/12 17:41:42 by esukava          ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   render_output.c									:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: jkoskela <jkoskela@student.hive.fi>		+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/09/09 05:30:06 by jkoskela		  #+#	#+#			 */
+/*   Updated: 2020/09/12 18:23:01 by jkoskela		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "fillit.h"
@@ -16,28 +16,18 @@ char		*bitoa(uint64_t nb, size_t len)
 {
 	size_t		i;
 	char		*str;
-	
+
 	i = 0;
-	
-//	printf("bitoa, lol");
 	str = ft_strnew(len);
 	str[len + 1] = '\0';
 	while (i < len)
 	{
 		if (checkbit(nb, i) == 1)
-		{
 			str[i] = '1';
-	//		write (1, "x", 1);
-		}
 		else
-		{
 			str[i] = '0';
-	//		write (1, "y", 1);
-		}
 		i++;
 	}
-
-//	printf("str in bitoa is = %s", str);
 	return (str);
 }
 
@@ -49,12 +39,8 @@ void		render_line(char *dest, char *src, char c)
 
 	while (dest[i] != '\0')
 	{
-	//	write(1, "qq", 2);
 		if (src[i] == '1')
-		{
-	//		write(1, "JOE", 3);
 			dest[i] = c;
-		}
 		i++;
 	}
 }
@@ -89,48 +75,44 @@ void			render_output(t_program *PROGRAM, t_dlist *output)
 	j = 0;
 	size = PROGRAM->BOARD->h;
 	c = 'A';
-	array = (char **)malloc(sizeof(char) *  size);
+	array = (char **)malloc(sizeof(char) *  size + 1);
 	printf("\nOutput list at exit:\n\n");
-//	field_list_print(output);
+	field_list_print(output);
 	bcnt = dl_len(output);
-	printf("\e\n[1;38mStart board xy = %d\n\n\e[0m", (int)ft_sqrt(PROGRAM->BLOCK_COUNT * 4) - 1);
-	printf("\e[1;38mOutput count at exit: %zu\n\e[0m", bcnt);
-	printf("\e[1;38m\nBoard at exit(size %zu):\n\n\e[0m", PROGRAM->BOARD->w);
-	printf("PROGRAM->BOARD = \n");
+	printf("\e\n[1;34mStart board xy: %d\n\n\e[0m", (int)ft_sqrt(PROGRAM->BLOCK_COUNT * 4) - 1);
+	printf("\e[1;34mOutput count at exit: %zu\n\e[0m", bcnt);
+	printf("\e[1;34m\nBoard at exit(size %zu):\n\n\e[0m", PROGRAM->BOARD->w);
+	printf("board: \n\n");
 	bf_print(PROGRAM->BOARD);
 	printf("\n");
-	printf("output render = \n");
-         while (i < size)
-         {
-//			write(1, "1", 1);
-        	array[i] = ft_strnew(size + 2);
-         	array[i][size] = '\n';
-         	array[i][size + 1] = '\0';
-         	while (j < size)
-         	{
-         		array[i][j] = '.';
-         		j++;
-         	}
-         	j = 0;
-         	i++;
-         }
-         i = 0;
-		 while (i < PROGRAM->BLOCK_COUNT - 1) // -1 added to repair segfault
-		{	
-//			write(1, "2", 1);
-			tmp = output->content;
-         	render(tmp, array, c);
-         	output = output->next;
-         	i++;
-			c++;
-         }
-		 write(1, "\n", 1);
-         i = 0;
-         while (i < size)
-         {
-			
-//			write(1, "3", 1);
-         	ft_putstr(array[i]);
-         	i++;
-         }
+	printf("output render: \n\n");
+	while (i < size)
+	{
+		array[i] = ft_strnew(size + 2);
+		array[i][size] = '\n';
+		array[i][size + 1] = '\0';
+		while (j < size)
+		{
+			array[i][j] = '.';
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	i = 0;
+	while (i < PROGRAM->BLOCK_COUNT)
+	{
+		tmp = output->content;
+		render(tmp, array, c);
+		if (output->next)
+			output = output->next;
+		i++;
+		c++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		ft_putstr(array[i]);
+		i++;
+	}
 }
